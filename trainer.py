0eed887a8bbfa555c7ad9b38323ad8184244a553
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-05-08 23:19:58 +0530

    this file code using model traning

--- trainer.py
+++ trainer.py
@@ -6,10 +6,10 @@ import json
 with open('data.json','r') as file:
     data = json.load(file)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.1)
+optimizer = optim.Adam(model.model.parameters(), lr=0.1)
 for epoch in range(2):
     for qus,ans in data:
-        out,sof = model(qus.split(),["start","end"])
+        out,sof = model.model(qus.split(),["start","end"])
         lables = [vocab[ans.split()[i]] for i in range(len(out))]
         loss = criterion(torch.stack(sof),torch.tensor(lables))
         optimizer.zero_grad()
