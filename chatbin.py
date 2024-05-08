8f20cbaa2c7ed54a1a0721d89be388e7d80d61f6
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-05-09 00:18:38 +0530

    using our model for prompting your own data

--- chatbin.py
+++ chatbin.py
@@ -7,6 +7,7 @@ ffn_hidden = 2048
 drop_prob = 0.1
 n_layers = 6
 
+t = "test"
 if t == "train":
   state_dict = torch.load('model.pt')
   model = tersformer(d_model,n_head,n_layers,drop_prob,vocab,ffn_hidden)
