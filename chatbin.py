3b1077c3599b87e1e2f84769977d0f97467a0481
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-05-09 00:14:48 +0530

    using our model for prompting your own data

--- chatbin.py
+++ chatbin.py
@@ -1,4 +1,5 @@
 import torch
+import model as mod
 qus = "hay hello how are you"
 d_model = 512
 n_head = 8
@@ -6,12 +7,14 @@ ffn_hidden = 2048
 drop_prob = 0.1
 n_layers = 6
 
-state_dict = torch.load('model.pt')
-
-model = tersformer(d_model,n_head,n_layers,drop_prob,vocab,ffn_hidden)
-
-model.load_state_dict(state_dict)
-
-out,sof = model(qustion.split(),["start","end"],q_len)
-ans = ' '.join(out)
-print(ans)
+if t == "train":
+  state_dict = torch.load('model.pt')
+  model = tersformer(d_model,n_head,n_layers,drop_prob,vocab,ffn_hidden)
+  model.load_state_dict(state_dict)
+  out,sof = model(qustion.split(),["start","end"],q_len)
+  ans = ' '.join(out)
+  print(ans)
+else:
+  out,sof = mod.model(qustion.split(),["start","end"],q_len)
+  ans = ' '.join(out)
+  print(ans)
