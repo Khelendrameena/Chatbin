cd6254556605567225e574461f623cc696963aa2
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-05-09 21:28:34 +0530

    using our model for prompting your own data

--- chatbin.py
+++ chatbin.py
@@ -1,20 +1,3 @@
-import torch
-import model as mod
-d_model = 512
-n_head = 8
-ffn_hidden = 2048
-drop_prob = 0.1
-n_layers = 6
-
-def chatbin(t,qus,q_len):
-  if t == "train": 
-    model = mod.tersformer(d_model,n_head,n_layers,drop_prob,mod.vocab,ffn_hidden)
-    model.load_state_dict(torch.load('model.pt'))
-    model.evel
-    out,sof = model(qus.split(),["start","end"],q_len)
-    ans = ' '.join(out)
-    print(ans)
-  else:
-    out,sof = mod.model(qus.split(),["start","end"],q_len)
-    ans = ' '.join(out)
-    print(ans)
+import model as chatbin
+chatbin.trainer()
+chatbin.chatbin("train","hay hello how are you",10)
