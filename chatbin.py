10123cb2549a0cf0d78f03e9a23456496722e449
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-05-09 11:15:49 +0530

    using our model for prompting your own data

--- chatbin.py
+++ chatbin.py
@@ -8,9 +8,8 @@ n_layers = 6
 
 def chatbin(t,qus,q_len):
   if t == "train":
-    state_dict = torch.load('model.pt')
-    model = mod.model
-    model.load_state_dict(state_dict)
+    model.load_state_dict(torch.load('model.pt'))
+    model.evel()
     out,sof = mod.model(qus.split(),["start","end"],q_len)
     ans = ' '.join(out)
     print(ans)
